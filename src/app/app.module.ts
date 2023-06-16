0201f43f56d0043ef0ee416b07ba5aff6e28f395
Ihor <djigorsan@gmail.com>
2023-06-16 11:33:13 +0300

    deleted a copy of the folder with converter component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,8 +6,8 @@ import {AppRoutingModule} from './app-routing.module'
 import {FormsModule} from '@angular/forms'
 import {AppComponent} from './app.component'
 import {HttpClientModule} from '@angular/common/http'
-import {HeaderComponent} from './components/header/header.component';
-import { CurrencyConverterComponent } from './currency-converter/currency-converter.component'
+import {HeaderComponent} from './components/header/header.component'
+import {CurrencyConverterComponent} from './components/currency-converter/currency-converter.component'
 
 @NgModule({
 	declarations: [AppComponent, HeaderComponent, CurrencyConverterComponent],
